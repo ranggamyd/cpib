6e8bffc6adfc85a5e41db2cec3dfbdb88e74df9d
wikywildan <wikywildan@gmail.com>
2022-11-04 14:54:53 +0700

    login

---
+++ application/views/auth/login.php
@@ -0,0 +1,51 @@
+
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block "><img src="<?= base_url('assets/img/logo_bkipm2.png') ?>" alt=""></div>
+                            <div class="col-lg-6 d-flex align-items-center">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat datang di Sistem CPIB BKIPM</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user"
+                                                id="email" name="email"
+                                                placeholder="Silahkan Masukkan email anda">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password" name="password" placeholder="Password">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <hr>
+                                    </form>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Lupa Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('auth/registration') ?>">Buat Akun!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
---
+++ application/views/auth/registration.php
@@ -0,0 +1,51 @@
+
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block "><img src="<?= base_url('assets/img/logo_bkipm2.png') ?>" alt=""></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Daftar Akun Baru</h1>
+                            </div>
+                            <form class="user">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" name="nama" id="nama"
+                                        placeholder="Nama Lengkap">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="email" name="email"
+                                        placeholder="Alamat Email">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="password1" name="password1" placeholder="Masukkan Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="password2" name="password2" placeholder="Ulangi Masukkan Password">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    Daftar
+                                </button>
+                                <hr>
+                            </form>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Lupa Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="<?= base_url('auth/') ?>">Sudah Punya Akun? Masuk!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
