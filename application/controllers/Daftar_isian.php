0e007d90b2aeceeaac331e56b0d42264ad5e27e6
jerimaulana <jerimaulana44@gmail.com>
2022-11-10 21:21:04 +0700

    edit

--- application/controllers/Daftar_isian.php
+++ application/controllers/Daftar_isian.php
@@ -27,10 +27,119 @@ class Daftar_isian extends CI_Controller
 
     public function index()
     {
+        $data['kd_daftar_isian_auto'] = $this->daftar_isian_model->kd_daftar_isian_auto();
         $data['daftar_isian'] = $this->daftar_isian_model->semuaDaftar_isian();
 
         $data['title'] = 'Daftar Isian';
         $this->loadView('daftar_isian', $data);
+    }
+
+    public function tambah_kategori()
+    {
+        $this->form_validation->set_rules('kd_daftar_isian', 'Kode Daftar Isian', 'required');
+        $this->form_validation->set_rules('nama_isian', 'Nama Kategori', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->session->set_flashdata('gagal', 'Gagal menambahkan !');
+            $this->session->set_flashdata('hasModalID', 'tambah_kategori');
+            $this->index();
+        } else {
+            if ($this->daftar_isian_model->tambah_kategori()) {
+                $this->session->set_flashdata('sukses', 'Berhasil menambahkan !');
+                redirect('daftar_isian');
+            } else {
+                $this->session->set_flashdata('gagal', 'Gagal menambahkanas !');
+                $this->index();
+            }
+        }
+    }
+
+    public function ubah_kategori()
+    {
+        $kd_daftar_isian = $this->input->post('kd_daftar_isian');
+
+        $this->form_validation->set_rules('kd_daftar_isian', 'Kode Daftar Isian', 'required');
+        $this->form_validation->set_rules('nama_isian', 'Nama Kategori', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->session->set_flashdata('gagal', 'Gagal mengubah !');
+            $this->session->set_flashdata('hasModalID', 'ubah_kategori-' . $kd_daftar_isian);
+            $this->index();
+        } else {
+            if ($this->daftar_isian_model->ubah_kategori()) {
+                $this->session->set_flashdata('sukses', 'Berhasil mengubah !');
+                redirect('daftar_isian');
+            } else {
+                $this->session->set_flashdata('gagal', 'Gagal mengubah !');
+                $this->index();
+            }
+        }
+    }
+
+    public function hapus_kategori($kd_daftar_isian)
+    {
+        if ($this->daftar_isian_model->hapus_kategori($kd_daftar_isian)) {
+            $this->session->set_flashdata('sukses', 'Berhasil menghapus !');
+            redirect('daftar_isian');
+        } else {
+            $this->session->set_flashdata('gagal', 'Gagal menghapus !');
+            $this->index();
+        }
+    }
+
+    public function tambah_isian()
+    {
+        $this->form_validation->set_rules('kd_daftar_isian', 'Kode Kategori', 'required');
+        $this->form_validation->set_rules('nama_subisian', 'Nama Isian', 'required');
+        $this->form_validation->set_rules('acuan', 'Acuan', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->session->set_flashdata('gagal', 'Gagal menambahkan !');
+            $this->session->set_flashdata('hasModalID', 'tambah_isian');
+            $this->index();
+        } else {
+            if ($this->daftar_isian_model->tambah_isian()) {
+                $this->session->set_flashdata('sukses', 'Berhasil menambahkan !');
+                redirect('daftar_isian');
+            } else {
+                $this->session->set_flashdata('gagal', 'Gagal menambahkan !');
+                $this->index();
+            }
+        }
+    }
+
+    public function ubah_isian()
+    {
+        $id = $this->input->post('id');
+
+        $this->form_validation->set_rules('kd_daftar_isian', 'Kode Kategori', 'required');
+        $this->form_validation->set_rules('nama_subisian', 'Nama Isian', 'required');
+        $this->form_validation->set_rules('acuan', 'Acuan', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->session->set_flashdata('gagal', 'Gagal mengubah !');
+            $this->session->set_flashdata('hasModalID', 'ubah_isian-' . $id);
+            $this->index();
+        } else {
+            if ($this->daftar_isian_model->ubah_isian()) {
+                $this->session->set_flashdata('sukses', 'Berhasil mengubah !');
+                redirect('daftar_isian');
+            } else {
+                $this->session->set_flashdata('gagal', 'Gagal mengubah !');
+                $this->index();
+            }
+        }
+    }
+
+    public function hapus_isian($id)
+    {
+        if ($this->daftar_isian_model->hapus_isian($id)) {
+            $this->session->set_flashdata('sukses', 'Berhasil menghapus !');
+            redirect('daftar_isian');
+        } else {
+            $this->session->set_flashdata('gagal', 'Gagal menghapus !');
+            $this->index();
+        }
     }
 
     // public function tambah_ajuan()
--- application/models/Daftar_isian_model.php
+++ application/models/Daftar_isian_model.php
@@ -21,9 +21,93 @@ class Daftar_isian_model extends CI_Model
         return $kddaftar_isian;
     }
 
+    public function kd_sub_daftar_isian_auto()
+    {
+        $this->db->select('RIGHT(daftar_isian.kd_daftar_isian,3) as kd_daftar_isian', FALSE);
+        $this->db->order_by('kd_daftar_isian', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('daftar_isian');
+        if ($query->num_rows() <> 0) {
+            $data = $query->row();
+            $kd = intval($data->kd_daftar_isian) + 1;
+        } else {
+            $kd = 1;
+        }
+        $batas = str_pad($kd, 3, "0", STR_PAD_LEFT);
+        $kddaftar_isian = "CKR-" . $batas;
+        return $kddaftar_isian;
+    }
+
     public function semuaDaftar_isian()
     {
         return $this->db->get('daftar_isian')->result_array();
+    }
+
+    public function tambah_kategori()
+    {
+        $data = [
+            'kd_daftar_isian' => $this->input->post('kd_daftar_isian'),
+            'nama_isian' => $this->input->post('nama_isian'),
+            'deskripsi' => $this->input->post('deskripsi'),
+        ];
+
+        if ($this->db->insert('daftar_isian', $data)) return TRUE;
+    }
+
+    public function ubah_kategori()
+    {
+        $kd_daftar_isian = $this->input->post('kd_daftar_isian');
+
+        $data = [
+            'kd_daftar_isian' => $kd_daftar_isian,
+            'nama_isian' => $this->input->post('nama_isian'),
+            'deskripsi' => $this->input->post('deskripsi'),
+        ];
+
+        if ($this->db->update('daftar_isian', $data, ['kd_daftar_isian' => $kd_daftar_isian])) return TRUE;
+    }
+
+    public function hapus_kategori($kd_daftar_isian)
+    {
+        if (!$this->db->delete('daftar_isian', ['kd_daftar_isian' => $kd_daftar_isian])) return FALSE;
+        if ($this->db->delete('sub_daftar_isian', ['kd_daftar_isian' => $kd_daftar_isian])) return TRUE;
+    }
+
+    public function tambah_isian()
+    {
+        $data = [
+            'kd_daftar_isian' => $this->input->post('kd_daftar_isian'),
+            'nama_subisian' => $this->input->post('nama_subisian'),
+            'is_minor' => $this->input->post('is_minor') ? 1 : 0,
+            'is_mayor' => $this->input->post('is_mayor') ? 1 : 0,
+            'is_serius' => $this->input->post('is_serius') ? 1 : 0,
+            'is_kritis' => $this->input->post('is_kritis') ? 1 : 0,
+            'acuan' => $this->input->post('acuan'),
+        ];
+
+        if ($this->db->insert('sub_daftar_isian', $data)) return TRUE;
+    }
+
+    public function ubah_isian()
+    {
+        $id = $this->input->post('id');
+
+        $data = [
+            'kd_daftar_isian' => $this->input->post('kd_daftar_isian'),
+            'nama_subisian' => $this->input->post('nama_subisian'),
+            'is_minor' => $this->input->post('is_minor') ? 1 : 0,
+            'is_mayor' => $this->input->post('is_mayor') ? 1 : 0,
+            'is_serius' => $this->input->post('is_serius') ? 1 : 0,
+            'is_kritis' => $this->input->post('is_kritis') ? 1 : 0,
+            'acuan' => $this->input->post('acuan'),
+        ];
+
+        if ($this->db->update('sub_daftar_isian', $data, ['id' => $id])) return TRUE;
+    }
+
+    public function hapus_isian($id)
+    {
+        if ($this->db->delete('sub_daftar_isian', ['id' => $id])) return TRUE;
     }
 
     // public function tambah()
