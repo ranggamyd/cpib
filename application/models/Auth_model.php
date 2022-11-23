3a6cade4ef6c03a5c9915cc325b3fe2c853c7bc3
wikywildan <wikywildan@gmail.com>
2022-11-23 19:15:16 +0700

    uhuy

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -43,12 +43,14 @@ class Auth_model extends CI_Model
 		$phone = $this->input->post('phone');
 		$email = $this->input->post('email');
 		$password = md5($this->input->post('password1'));
+		$avatar = 'default_avatar.jpg';
 
 		$supplier = [
 			'kd_supplier' => $kd_supplier,
 			'nama_miniplant' => $name,
 			'no_telp' => $phone,
 			'email' => $email,
+			'avatar' => $avatar
 		];
 
 		if (!$this->db->insert('suppliers', $supplier)) return FALSE;
